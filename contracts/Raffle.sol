237d7bee4b6ff741f8c8644407699aa2faf8d954
NullSploit01 <harshaldharmik099@gmail.com>
2023-09-21 09:48:34 +0530

    add basic raffle contract setup

---
+++ contracts/Raffle.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+error Raffle__NotEnoughEth();
+
+contract Raffle {
+  uint256 private immutable i_ticketPrice;
+  address payable[] private s_participants;
+
+  constructor(uint256 ticketPrice) {
+    i_ticketPrice = ticketPrice;
+  }
+
+  function enter() public payable {
+    if (msg.value < i_ticketPrice) {
+      revert Raffle__NotEnoughEth();
+    }
+  }
+
+  function getTicketPrice() public view returns (uint256) {
+    return i_ticketPrice;
+  }
+
+  function getParticipant(uint256 index) public view returns (address) {
+    return s_participants[index];
+  }
+}
