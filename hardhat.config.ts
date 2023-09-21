d862f76a56e14298e9f06593efef25df9e0d5956
NullSploit01 <harshaldharmik099@gmail.com>
2023-09-21 09:35:32 +0530

    add hardhat-deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import '@nomicfoundation/hardhat-toolbox'
+import 'dotenv/config'
+import 'hardhat-deploy'
+import 'hardhat-gas-reporter'
 import { HardhatUserConfig } from 'hardhat/config'
+import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19'
